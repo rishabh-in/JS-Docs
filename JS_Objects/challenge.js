6ef49207262ed415e09249f5fae3efc8d44ddcca
Rishabh <13rishabhtiwari@gmail.com>
2022-03-13 11:51:47 +0530

    First Commit - Objects

---
+++ JS_Objects/challenge.js
@@ -0,0 +1,38 @@
+// Create a object named Book;
+
+// Using class
+
+class Book {
+    constructor(bookName, noOfPages) {
+        this.bookName = bookName;
+        this.noOfPages = noOfPages;
+    }
+    changePageNumber = function(pageNumber) {
+        this.noOfPages = pageNumber
+    }
+}
+
+const newBook = new Book('Physics', 108);
+
+console.log(newBook);
+console.log(newBook['bookName']);
+console.log(newBook['noOfPages']);
+
+newBook.changePageNumber(200);
+
+console.log(newBook['bookName']);
+console.log(newBook['noOfPages']);
+
+
+// Using object constructor
+
+function NewBook(bookName, noOfPages) {
+    this.bookName = bookName;
+    this.noOfPages = noOfPages;
+    this.changeNoOfPages = function(page) {
+        this.noOfPages = page;
+    }
+}
+
+let bookTwo = new NewBook('Chemistry', 200);
+console.log(bookTwo);
